8a88981f8f92d52a28dd284c3c0ebbf6eea1b104
MrBacon <fabianmachowski@gmail.com>
2023-11-22 10:03:52 +0000

    Update AES-256.py

--- AES-256.py
+++ AES-256.py
@@ -3,7 +3,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding, hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import os
-from time import sleep
 import base64
 import argparse
 import getpass
